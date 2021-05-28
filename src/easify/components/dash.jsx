62e82d80f3122fa36b6523835fb08c806d835df2
sam3kent <sam12kent@gmail.com>
2021-05-27 20:34:56 -0700

    Built settings button component and the css files + set up a button(currently commented out) for when/if we add dark/light mode

--- src/easify/components/dash.jsx
+++ src/easify/components/dash.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect, useRef} from 'react';
 import styles from '../styles/Dash.module.css';
 import models from '../models/models';
 import YearCard from './year-card';
+import Settings from './settings';
 
 /**
  * This file will contain all necessary UI+Implementation
@@ -10,40 +11,17 @@ import YearCard from './year-card';
  * @returns Dash Object
  */
 export default function Dash({ data, changePage }) {
-  const [showPopup, setShowPopup] = useState(false);
   const [resetJournal, setResetJournal] = useState(false);
+  //const [theme, setTheme] = useState(false);
 
   return (
     <div className={styles.wrap}>
-      <div className={styles.header}>
-        <div>
-          <p className={styles.settingsbtn} aria-hidden="true" onClick={() => setShowPopup(!showPopup)}>Settings</p>
-          {showPopup ? (
-            <div className={styles.popup}>
-              <p aria-hidden="true" onClick={() => setResetJournal(!resetJournal)}>
-                Reset Journal
-              </p>
-            </div>
-          ) : null}
-        </div>
-      </div>
-      {resetJournal ? (
-        <div className={styles.warning}>
-          <p>Are You Sure?</p>
-          <div className={styles.warnbtnwrap}>
-            <p
-              aria-hidden="true"
-              onClick={() => {
-                data.set([]);
-                setResetJournal(false);
-              }}
-            >
-              YES
-            </p>
-            <p id="warnno" aria-hidden="true" onClick={() => setResetJournal(false)}>NO</p>
-          </div>
-        </div>
-      ) : null}
+      <Settings
+        data={{val: data, set: data.set}}
+        rj={resetJournal}
+        setrj={() => setResetJournal(!resetJournal)}
+      />
+      
       <div className={resetJournal ? styles.welcomeh : styles.welcome}>
         {/** Order of HTML reversed to accomodate
          * flex-column-reverse property */}
@@ -73,7 +51,7 @@ export default function Dash({ data, changePage }) {
           }])}
         className={resetJournal ? styles.newyearbtnh : styles.newyearbtn}
       >
-        <img src="/icons/plussign.svg" alt="plus sign" />
+        <img className={styles.plusimg}src="/icons/plussign.svg" alt="plus sign" />
         <p>Add Year</p>
       </div>
 
